aeffbea194cb548d054e52d1f50e04eedf1435c9
Robert Kalmar <rkalmar@cloudera.com>
2021-12-04 22:46:38 +0100

    Finished day 3

---
+++ Day3/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var inputs = AocUtils.AocUtils.ReadLinesFromAssemblyResource(assembly, "Day3.input.txt")
+                .Select(line => line.Select(c => c == '1' ? 1 : 0).ToList())
+                .ToList();
+
+            Console.WriteLine($"power consumption: {Part1(inputs)}");
+            Console.WriteLine($"life support rating: {Part2(inputs)}");
+        }
+
+        public static int Part1(List<List<int>> inputs)
+        {
+            var bitCounts = inputs
+                .Cast<IEnumerable<int>>()
+                .Aggregate((acc, bits) => acc.Zip(bits).Select(values => values.First + values.Second))
+                .Reverse()
+                .ToList();
+
+            var mostCommonBits = bitCounts
+                .Select(bitCount => bitCount >= inputs.Count - bitCount ? 1 : 0)
+                .ToList();
+
+            var leastCommonBits = mostCommonBits
+                .Select(bitValue => 1 - bitValue)
+                .ToList();
+
+            var gamma = BitValuesToInt(mostCommonBits);
+            var epsilon = BitValuesToInt(leastCommonBits);
+            Console.WriteLine($"gamma: {gamma}, epsilon: {epsilon}");
+            return gamma * epsilon;
+        }
+
+        public static int Part2(List<List<int>> inputs)
+        {
+            var oxigenGeneratorBits = GetRateBits(inputs, true);
+            var co2ScrubberBits = GetRateBits(inputs, false);
+
+            var oxigenGeneratorRate = BitValuesToInt(oxigenGeneratorBits.AsEnumerable().Reverse());
+            var co2ScrubberRate = BitValuesToInt(co2ScrubberBits.AsEnumerable().Reverse());
+
+            Console.WriteLine($"oxigen generator rate: {oxigenGeneratorRate}, CO2 scrubber rate: {co2ScrubberRate}");
+            return oxigenGeneratorRate * co2ScrubberRate;
+        }
+
+        public static List<int> GetRateBits(List<List<int>> inputs, bool forOxigenGenerator)
+        {
+            var copyOfInput = new List<List<int>>(inputs);
+            var valueOfMostCommonBit = forOxigenGenerator ? 1 : 0;
+            for (int x = 0; x < copyOfInput.First().Count; ++x)
+            {
+                var onBitCount = OnBitsAt(copyOfInput, x);
+                int valueToKeep = onBitCount >= copyOfInput.Count - onBitCount
+                    ? valueOfMostCommonBit
+                    : 1 - valueOfMostCommonBit;
+                copyOfInput.RemoveAll(bits => bits.ElementAt(x) != valueToKeep);
+                if (copyOfInput.Count == 1)
+                {
+                    return copyOfInput.First();
+                }
+            }
+
+            throw new ArgumentException("Invalid arguments", nameof(inputs));
+        }
+
+        static int OnBitsAt(List<List<int>> bitsList, int index) => bitsList
+                .Where(bits => bits.ElementAt(index) == 1)
+                .Count();
+
+        public static int BitValuesToInt(IEnumerable<int> bitValues) => bitValues
+            .Select((value, index) => value << index)
+            .Aggregate((acc, value) => acc | value);
+    }
+}
---
+++ Day3Tests/DayTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using Day3;
+
+namespace Day3Tests
+{
+    [TestClass]
+    public class DayTests
+    {
+        static readonly List<string> testInput = new List<string>
+        {
+            "00100",
+            "11110",
+            "10110",
+            "10111",
+            "10101",
+            "01111",
+            "00111",
+            "11100",
+            "10000",
+            "11001",
+            "00010",
+            "01010",
+        };
+
+        [TestMethod]
+        public void Part1Test()
+        {
+            var inputs = testInput
+                .Select(line => line.Select(c => c == '1' ? 1 : 0).ToList())
+                .ToList();
+
+            Assert.AreEqual(198, Program.Part1(inputs));
+        }
+
+        [TestMethod]
+        public void Part2Test()
+        {
+            var inputs = testInput
+                .Select(line => line.Select(c => c == '1' ? 1 : 0).ToList())
+                .ToList();
+
+            var oxigenRateBits = Program.GetRateBits(inputs, true);
+            var co2RateBits = Program.GetRateBits(inputs, false);
+            CollectionAssert.AreEqual(new List<int> { 1, 0, 1, 1, 1 }, oxigenRateBits);
+            CollectionAssert.AreEqual(new List<int> { 0, 1, 0, 1, 0 }, co2RateBits);
+
+            var oxigenGeneratorRate = Program.BitValuesToInt(oxigenRateBits.AsEnumerable().Reverse());
+            var co2ScrubberRate = Program.BitValuesToInt(co2RateBits.AsEnumerable().Reverse());
+            Assert.AreEqual(230, oxigenGeneratorRate * co2ScrubberRate);
+        }
+    }
+}
