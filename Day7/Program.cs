5bec2f03420acf0fbed1e88fc3e09b72e3f6673d
Robert Kalmar <rkalmar@cloudera.com>
2021-12-09 17:03:40 +0100

    Finished day 7

---
+++ Day7/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Day7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var input = Input.Get(AocUtils.AocUtils.ReadLinesFromAssemblyResource(assembly, "Day7.input.txt").First());
+            Console.WriteLine($"Least fuel consumption with constant rate: {CrabSubmarines.FindLeastFuelConsumptionContantRate(input)}");
+            Console.WriteLine($"Least fuel consumption with variable rate: {CrabSubmarines.FindLeastFuelConsumptionVariableRate(input)}");
+        }
+    }
+
+    public static class CrabSubmarines
+    {
+        public static int FindLeastFuelConsumptionContantRate(SortedDictionary<int, int> input) => RangeOfInput(input)
+            .Select(i => FuelConsumptionConstantRate(i, input))
+            .Min();
+
+        public static int FindLeastFuelConsumptionVariableRate(SortedDictionary<int, int> input) => RangeOfInput(input)
+            .Select(i => FuelConsumptionVariableRate(i, input))
+            .Min();
+
+        static IEnumerable<int> RangeOfInput(SortedDictionary<int, int> input) =>
+            Enumerable.Range(input.Min(pair => pair.Key), input.Max(pair => pair.Key));
+
+        static int Distance(int i, int j) => Math.Max(i, j) - Math.Min(i, j);
+
+        static int FuelConsumptionConstantRate(int i, SortedDictionary<int, int> input) =>
+            input.Select(pair => Distance(i, pair.Key) * pair.Value).Sum();
+
+        static int FuelConsumptionVariableRate(int i, SortedDictionary<int, int> input) =>
+            input.Select(pair =>
+            {
+                var distance = Distance(i, pair.Key);
+                var fuelConsumption = distance * (distance + 1) / 2;
+                return fuelConsumption * pair.Value;
+            })
+            .Sum();
+    }
+
+    public static class Input
+    {
+        public static SortedDictionary<int, int> Get(string input) => new SortedDictionary<int, int>(input.Split(',')
+            .Select(int.Parse)
+            .GroupBy(num => num, (num, values) => KeyValuePair.Create(num, values.Count()))
+            .ToDictionary(pair => pair.Key, pair => pair.Value));
+    }
+}
---
+++ Day7Tests/Tests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using Day7;
+
+namespace Day7Tests
+{
+    [TestClass]
+    public class Tests
+    {
+        static readonly string input = "16,1,2,0,4,2,7,1,2,14";
+
+        [TestMethod]
+        public void Part1()
+        {
+            var positions = Input.Get(input);
+            Assert.AreEqual(37, CrabSubmarines.FindLeastFuelConsumptionContantRate(positions));
+        }
+
+        [TestMethod]
+        public void Part2()
+        {
+            var positions = Input.Get(input);
+            Assert.AreEqual(168, CrabSubmarines.FindLeastFuelConsumptionVariableRate(positions));
+        }
+    }
+}
